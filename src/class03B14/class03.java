c566f60c374f0da72fbfce4de94b3cc910e0f7eb
Stallionz2810 <stallionz2810@gmail.com>
2022-09-27 19:51:48 -0400

    Second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -3,6 +3,7 @@ package class03B14;
 public class class03 {
 	public static void main(String[] args) {
 		System.out.println("hey this is a git class");
+		System.out.println("hey this is a change in my project");
 		
 		
 	}
